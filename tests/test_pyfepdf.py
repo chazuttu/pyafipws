ca9477cbdb8fbbd59944eee0c8527a29d86b3cf6
UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>
2021-08-14 06:02:15 +0530

    testing_3
    Signed-off-by: UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>

--- tests/test_pyfepdf.py
+++ tests/test_pyfepdf.py
@@ -317,7 +317,10 @@ def test_main_grabar():
     main()
     #TO-DO : compare the generated facturas.txt with the original file
     f1 = open("facturas.txt", "r")
-    f2 = open("tests/facturas.txt", "r")
+    if sys.version_info[0] < 3:
+        f2 = open("tests/facturas_py2.txt", "r")
+    else:
+        f2 = open("tests/facturas.txt", "r")
     d1 = f1.readlines()
     d2 = f2.readlines()
     f1.close()
